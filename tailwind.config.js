14976d4dbcec5fb48f6105ffd66b486e8fd86754
cephus <pnakitaren2@gmail.com>
2024-03-03 08:53:00 -0500

    task: removed the hero 3d model

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        accent: "#e5de00",
         primary: "#050816",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
-        accent: "#e5de00",
         grade: "#c30010"
       },
       boxShadow: {
